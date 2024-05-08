24f08b58da0d0d14092c20f2f882b6aac0191cee
slytherdor <145475795+slytherdor@users.noreply.github.com>
2024-05-08 11:01:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
 import pygame
 from red_heart import Red_Heart
+from play_button import Play_Button
 
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont('Comic Sans', 13)
-big_font = pygame.font.SysFont('Comic Sans', 16)
-pygame.display.set_caption("Coin Collector!")
+my_font = pygame.font.SysFont('Consolas', 15)
+pygame.display.set_caption("NPC Battle!")
 
-heart = pygame.image.load('red heart.png')
-
-health = Red_Heart(40, 60)
-
-screen.blit(f.image, f.rect)
-
-keys = pygame.key.get_pressed()  # checking pressed keys
-if keys[pygame.K_d]:
-   f.move_direction("right")
-   start = True
-if keys[pygame.K_a]:
-   f.move_direction("left")
-   start = True
-if keys[pygame.K_s]:
-   f.move_direction("down")
-   start = True
-if keys[pygame.K_w]:
-   f.move_direction("up")
-   start = True
-
-
-size = (800, 1000)
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
+
+r = 50
+g = 0
+b = 100
+
+message = "Welcome to NPC Battle!"
+display_message = my_font.render(message, True, (255, 255, 255))
+message2 = "To start, please click on the play button below:"
+display_message2 = my_font.render(message2, True, (255, 255, 255))
+
+h = Red_Heart(1, 1)
+pb = Play_Button(1, 1)
+
+run = True
+# -------- Main Program Loop -----------
+while run:
+
+    # --- Main event loop
+    ## ----- NO BLIT ZONE START ----- ##
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            if h.rect.collidepoint(event.pos):
+                h.move(h.x + 3, h.y + 3)
+    ##  ----- NO BLIT ZONE END  ----- ##
+
+    ## FILL SCREEN, and BLIT here ##
+    screen.fill((r, g, b))
+    screen.blit(display_message, (0, 0))
+    screen.blit(display_message2, (200, 400))
+    screen.blit(h.image, h.rect)
+    pygame.display.update()
+    ## END OF WHILE LOOP
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+
+
+
+
+
--- play_button.py
+++ play_button.py
@@ -1,11 +1,11 @@
 import pygame
 
-class Red_Heart:
+class Play_Button:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("red heart.png")
+        self.image = pygame.image.load("play button.png")
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
